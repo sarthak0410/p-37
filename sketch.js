d08cf0e423e5428e006c63795923bd20904566ea
sarthak0410 <74004377+sarthak0410@users.noreply.github.com>
2021-03-20 11:25:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var canva;
+var gameState=0;
+var contestantCount;
+var database;
+var quiz;
+var question;
+var contestant;
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database=firebase.database();
+}
+
+
+function draw(){
+  background("pink");
+
+  
+}
